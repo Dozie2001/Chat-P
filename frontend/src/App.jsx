0f1567b5cf7aaa8e02da024d10a752b963020da5
Dozie2001 <davidchidozie402@gmail.com>
2024-01-26 18:45:05 +0100

    Setup the Routes on the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router,   Routes, Route } from 'react-router-dom';
+import { Join, Chat } from "./Components/Join.jsx";
+
+
+const App = () => {
+  return (
+    <Router>
+    <Routes>
+      <Route path='/' element={<Join />} />
+      <Route path='/chat' element={<Chat />} />
+    </Routes>
+  </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Join.jsx
@@ -0,0 +1,11 @@
+export const Join = () => {
+    return (
+    <h1> Join </h1>
+    )
+}
+
+export const Chat = () => {
+    return (
+    <h1> Chat </h1>
+    )
+};
