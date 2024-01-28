ffc3ec52247f96ee6526fa1ce7e915bcbaf059dc
Dozie2001 <davidchidozie402@gmail.com>
2024-01-28 12:47:30 +0100

    Setting up socket

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const router = require('./router')
+
+const PORT =  5001
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection!!!');
+
+    socket.on('diconnect', () => {
+        console.log('User has left!!!')
+    })
+})
+
+server.listen(PORT, () => console.log(`Server has started on http://localhost:${PORT}`))
+app.use(router);
+app.listen()
