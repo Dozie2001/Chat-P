2349a7d276848eb3b690fb10fbeaa0851a03dcdd
Dozie2001 <davidchidozie402@gmail.com>
2024-02-07 00:45:01 +0100

    Fixed up chat functionality but errors are still present

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const router = require('./router')
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users')
 
 const PORT =  5001
 
@@ -14,13 +15,41 @@ const io = socketio(server, {
 
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!!');
+    socket.on('join', ({ name, room}, callback)=> {
+        console.log(name, room)
+       try {
+        const { error, user} = addUser({id: socket.id, name, room});
 
-    socket.on('diconnect', () => {
-        console.log('User has left!!!')
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
+        socket.join(user.room);
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUserInRoom(user.room) });
+
+       } catch (error) {
+        console.error('Error in join event:', error);
+        callback()
+       }
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+    })
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+        console.log('User has left!!!');
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUserInRoom(user.room)});
+        }
+      
     })
 })
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on http://localhost:${PORT}`))
-app.use(router);
 app.listen()
